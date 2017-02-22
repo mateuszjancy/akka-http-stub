c71ee1b6e335dc28aaff1d3297b0b6fa600f56e3
Mateusz Jancy <mateusz.jancy@gmail.com>
2017-02-22 22:05:50 +0100

    Graph example.

--- src/main/scala/app/Service.scala
+++ src/main/scala/app/Service.scala
@@ -3,19 +3,16 @@ package app
 import java.util.concurrent.Executors
 
 import akka.NotUsed
-import akka.actor.ActorSystem
-import akka.http.scaladsl.Http
 import akka.http.scaladsl.common.{EntityStreamingSupport, JsonEntityStreamingSupport}
-import akka.http.scaladsl.server.Directives._
 import akka.http.scaladsl.marshallers.sprayjson.SprayJsonSupport
-import akka.http.scaladsl.model.{HttpRequest, HttpResponse}
+import akka.http.scaladsl.server.Directives._
 import akka.http.scaladsl.server.Route
-import akka.http.scaladsl.unmarshalling.Unmarshal
-import akka.stream.Materializer
-import akka.stream.scaladsl.Source
+import akka.stream.FlowShape
+import akka.stream.scaladsl.{Broadcast, Flow, GraphDSL, Source, ZipWith}
 import app.Repository.{Item, User}
 import spray.json.DefaultJsonProtocol
 
+import scala.collection.immutable.Seq
 import scala.concurrent.{ExecutionContext, Future}
 
 trait Service {
@@ -23,7 +20,9 @@ trait Service {
 }
 
 object Service {
+
   case class UsersWithItems(users: Seq[User], items: Seq[Item])
+  case class UserWithItem(users: Option[User], items: Option[Item])
 
   private implicit val ec = ExecutionContext.fromExecutor(Executors.newCachedThreadPool())
 
@@ -32,11 +31,8 @@ object Service {
     implicit val userJson = jsonFormat1(User)
     implicit val itemJson = jsonFormat1(Item)
     implicit val usersWithItemsJson = jsonFormat2(UsersWithItems)
+    implicit val userWithItemJson = jsonFormat2(UserWithItem)
     implicit val jsonStreamingSupport: JsonEntityStreamingSupport = EntityStreamingSupport.json()
-
-    def streamUsersWithItems: Route = get {
-
-    }
 
     def usersWithItems: Route = get {
       val userResponse: Future[Seq[User]] = Future(repository.users)
@@ -49,11 +45,43 @@ object Service {
       complete(result)
     }
 
+    def userWithItemStream: Route = get {
+      path("userwithitem" / IntNumber) { id =>
+        val userFlow: Flow[Int, Option[User], NotUsed] =
+          Flow[Int]
+            .map(id => repository.users.filter(_.name.endsWith(id.toString)))
+            .map(_.headOption)
+
+        val itemsFlow: Flow[Int, Option[Item], NotUsed] =
+          Flow[Int]
+            .map(id => repository.items.filter(_.name.endsWith(id.toString)))
+            .map(_.headOption)
+
+        val source = Source.single(id)
+
+        val flow: Flow[Int, UserWithItem, NotUsed] = Flow.fromGraph(GraphDSL.create() { implicit builder: GraphDSL.Builder[NotUsed] =>
+          import GraphDSL.Implicits._
+
+          val bcast = builder.add(Broadcast[Int](2))
+          val merge = builder.add(ZipWith[Option[User], Option[Item], UserWithItem]{case (user, item) => UserWithItem(user, item)})
+
+
+          bcast ~> userFlow  ~> merge.in0
+          bcast ~> itemsFlow ~> merge.in1
+
+          FlowShape(bcast.in, merge.out)
+        })
+
+        complete(source.via(flow))
+      }
+    }
+
+
     def items: Route = get {
       val response: Source[Item, NotUsed] = Source(repository.items)
       complete(response)
     }
 
-    override def routes = path("items")(items) ~ path("userswithitems")(usersWithItems)
+    override def routes = path("items")(items) ~ path("userswithitems")(usersWithItems) ~ userWithItemStream
   }
 }
